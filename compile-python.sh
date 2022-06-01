be28d03e0d45eb67ec6bfaea0426c4ee6dec7273
Julien Palard <julien@palard.fr>
2022-06-01 22:24:23 +0200

    Expedited release of Python3.11.0b3

--- compile-python.sh
+++ compile-python.sh
@@ -53,7 +53,7 @@ compile-pythons()
     compile-python 3.8.13 &
     compile-python 3.9.13 &
     compile-python 3.10.4 &
-    compile-python 3.11.0 b2 &
+    compile-python 3.11.0 b3 &
     wait
 }
 
