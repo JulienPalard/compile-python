1f427884504c9385639a7a54acf0abc490ebc408
Julien Palard <julien@palard.fr>
2022-08-19 15:35:55 +0200

    Python 3.10.6 is available

--- compile-python.sh
+++ compile-python.sh
@@ -52,7 +52,7 @@ compile-pythons()
     compile-python 3.7.13 &
     compile-python 3.8.13 &
     compile-python 3.9.13 &
-    compile-python 3.10.5 &
+    compile-python 3.10.6 &
     compile-python 3.11.0 rc1 &
     wait
 }
